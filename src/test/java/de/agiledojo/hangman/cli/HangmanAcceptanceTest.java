0d19d90e314aefecc7adba8cfb6b94a352b558ee
Christian Fischer <cf@fisch3r.de>
2019-02-16 17:49:06 +0100

    refactoring

--- src/test/java/de/agiledojo/hangman/cli/HangmanAcceptanceTest.java
+++ src/test/java/de/agiledojo/hangman/cli/HangmanAcceptanceTest.java
@@ -3,73 +3,39 @@ package de.agiledojo.hangman.cli;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import static de.agiledojo.hangman.cli.IO.initializeStdOut;
 import static de.agiledojo.hangman.cli.IO.enter;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
+import static de.agiledojo.hangman.cli.IO.startApplicationWithArgument;
+
 
 class HangmanAcceptanceTest {
 
-    private ByteArrayOutputStream stdOut;
-    private ExecutorService executor;
-    private boolean waitForOutput;
+
+    private IO io;
 
     @BeforeEach
     void setUp() {
-        stdOut = initializeStdOut();
+        io = IO.create();
     }
 
     @Test
     void showsPlaceHoldersWhenWordDoesNotContainProvidedLetter() {
         enter("q");
         startApplicationWithArgument("Secret");
-        assertOutputToBe("------\n");
+        io.assertOutputToBe("------\n");
     }
 
     @Test
     void showsMatchingLettersInBoard() {
         enter("s\ne");
         startApplicationWithArgument("Secret");
-        assertOutputToBe("S-----\nSe--e-\n");
+        io.assertOutputToBe("S-----\nSe--e-\n");
     }
 
     @Test
     void showsMessageWhenBoardIsComplete() {
         enter("t\nd\n");
         startApplicationWithArgument("TDD");
-        assertOutputToBe("T--\nTDD\nYou won!\n");
+        io.assertOutputToBe("T--\nTDD\nYou won!\n");
     }
 
-    private void assertOutputToBe(String output) {
-        waitForOutput = true;
-        Timer timer = new Timer("timeout");
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                stopWaitingForOutput();
-            }
-        },100);
-        boolean match = false;
-        while(waitForOutput && !match) {
-            match = stdOut.toString().equals(output);
-        }
-        if (!match)
-            fail("Expected: " + output + "\nActual: " + stdOut);
-    }
-
-    private void stopWaitingForOutput() {
-        waitForOutput = false;
-    }
-
-
-    private void startApplicationWithArgument(String argument) {
-        executor = Executors.newSingleThreadExecutor();
-        executor.submit(() -> Hangman.main(new String[]{argument}));
-    }
 }
--- src/test/java/de/agiledojo/hangman/cli/IO.java
+++ src/test/java/de/agiledojo/hangman/cli/IO.java
@@ -3,17 +3,64 @@ package de.agiledojo.hangman.cli;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-class IO {
-    static void enter(String input) {
+import static org.assertj.core.api.Assertions.fail;
 
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
+public class IO {
+
+    private boolean waitForOutput;
+    private final ByteArrayOutputStream stdOut;
+
+
+    public static IO create() {
+        return new IO(initializeStdOut());
     }
 
-    static ByteArrayOutputStream initializeStdOut() {
+
+
+    public static ByteArrayOutputStream initializeStdOut() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(baos);
         System.setOut(ps);
         return baos;
+    }
+
+    private IO(ByteArrayOutputStream stdOut) {
+        this.stdOut =stdOut;
+    }
+
+    public static void enter(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+
+    public void assertOutputToBe(String output) {
+        waitForOutput = true;
+        Timer timer = new Timer("timeout");
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                stopWaitingForOutput();
+            }
+        },100);
+        boolean match = false;
+        while(waitForOutput && !match) {
+            match = stdOut.toString().equals(output);
+        }
+        if (!match)
+            fail("Expected: " + output + "\nActual: " + stdOut);
+    }
+
+    private void stopWaitingForOutput() {
+        waitForOutput = false;
+    }
+
+
+    public static void startApplicationWithArgument(String argument) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(() -> Hangman.main(new String[]{argument}));
     }
 }

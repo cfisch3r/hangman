330139b6bca3b095ecb29e608af3f425d523871c
Christian Fischer <cf@fisch3r.de>
2019-03-06 18:30:19 +0100

    refactoring

--- src/test/java/de/agiledojo/hangman/HangmanAcceptanceTest.java
+++ src/test/java/de/agiledojo/hangman/HangmanAcceptanceTest.java
@@ -3,16 +3,36 @@ package de.agiledojo.hangman;
 import de.agiledojo.hangman.test.ApplicationRunner;
 import de.agiledojo.hangman.test.MockStdIn;
 import de.agiledojo.hangman.test.OutputListener;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HangmanAcceptanceTest {
 
+    private MockStdIn mockStdIn;
+    private OutputListener outputListener;
+
+    @BeforeEach
+    void setUpTestFixture() {
+        mockStdIn = MockStdIn.create();
+        outputListener = OutputListener.create();
+    }
+
     @Test
     void boardHasOnlyPlaceHolderWhenInputDoesNotMatch() {
-        MockStdIn mockStdIn = MockStdIn.create();
-        OutputListener outputListener = OutputListener.create();
-        ApplicationRunner.startApplicationWithArgument(Hangman::main,"Secret");
-        mockStdIn.enter("ö");
-        outputListener.assertOutputToBe("------\n");
+        startApplication("Secret");
+        enter("ö");
+        assertOutputToBe("------\n");
+    }
+
+    private void assertOutputToBe(String output) {
+        outputListener.assertOutputToBe(output);
+    }
+
+    private void enter(String line) {
+        mockStdIn.enter(line);
+    }
+
+    private ApplicationRunner.ApplicationStatus startApplication(String secret) {
+        return ApplicationRunner.startApplicationWithArgument(Hangman::main, secret);
     }
 }
